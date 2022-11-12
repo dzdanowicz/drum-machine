97a208417c0fb46d00fc61f0fffdc99f8552702c
dzdanowicz <contact@dzdanowicz.com>
2022-11-12 19:30:19 +0100

    Add drum sounds and sets.js file where sounds are assigned to keys

---
+++ sounds/sets.js
@@ -0,0 +1,14 @@
+const set = {
+  title: drum,
+  sounds: {
+    q: "clap.mp3",
+    w: "closed-hihat.mp3",
+    e: "open-hihat.mp3",
+    a: "splash-cymbal.mp3",
+    s: "ride-cymbal.mp3",
+    d: "crash-cymbal.mp3",
+    z: "kick.mp3",
+    x: "tom.mp3",
+    c: "snare.mp3",
+  },
+};
