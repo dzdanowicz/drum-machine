9048c55137d295c03585729f7bb14cc32917f028
dzdanowicz <contact@dzdanowicz.com>
2022-11-13 13:30:07 +0100

    Add src to audio element based on paths in sets.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,8 @@ import { set } from "/sounds/sets.js";
 const $pads = $(".drum-pad");
 const keys = [81, 87, 69, 65, 83, 68, 90, 88, 67];
 
+for (const [key, value] of Object.entries(set.sounds)) {
+  $("#" + key)[0].src = value;
+}
+
 $(document).keypress(function (event) {});
