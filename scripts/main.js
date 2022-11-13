db4c2a88926ffaf1325fb0c84ebc5d09e33f7bde
dzdanowicz <contact@dzdanowicz.com>
2022-11-13 13:59:07 +0100

    Fix data-num numbers to correct ones

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import { set } from "/sounds/sets.js";
 
 const $pads = $(".drum-pad");
-const keys = [81, 87, 69, 65, 83, 68, 90, 88, 67];
+const keys = [113, 119, 101, 97, 115, 100, 122, 120, 99];
 
 for (const [key, value] of Object.entries(set.sounds)) {
   $("#" + key)[0].src = value;
