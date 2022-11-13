01179de5170311ad386b48cb664fde55daca8279
dzdanowicz <contact@dzdanowicz.com>
2022-11-13 13:27:59 +0100

    Fix sound path to correct one

--- sounds/sets.js
+++ sounds/sets.js
@@ -1,15 +1,15 @@
 const set = {
   title: "drum",
   sounds: {
-    q: "clap.mp3",
-    w: "closed-hihat.mp3",
-    e: "open-hihat.mp3",
-    a: "splash-cymbal.mp3",
-    s: "ride-cymbal.mp3",
-    d: "crash-cymbal.mp3",
-    z: "kick.mp3",
-    x: "tom.mp3",
-    c: "snare.mp3",
+    q: "sounds/clap.mp3",
+    w: "sounds/closed-hihat.mp3",
+    e: "sounds/open-hihat.mp3",
+    a: "sounds/splash-cymbal.mp3",
+    s: "sounds/ride-cymbal.mp3",
+    d: "sounds/crash-cymbal.mp3",
+    z: "sounds/kick.mp3",
+    x: "sounds/tom.mp3",
+    c: "sounds/snare.mp3",
   },
 };
 
