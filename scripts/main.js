9d6f4acd3219b10eecc3a94c53311b4faa5e2895
dzdanowicz <contact@dzdanowicz.com>
2022-11-18 19:58:40 +0100

    Pressing keys performs an action on the correct div Fix for holding key, holding multiple keys and uppercase input

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,34 @@
 import { set } from "/sounds/sets.js";
 
-const $pads = $(".drum-pad");
 const keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
 
 for (const [key, value] of Object.entries(set.sounds)) {
   $("#" + key)[0].src = value;
 }
 
-function active(element) {}
+let pressedKeys = [];
 
-$(document).keypress(function (event) {
-  keys.forEach((key) => {
-    if (event.key === key) {
-      active($(`div[data-key=${key}]`), key);
+$(document).on("keyup", function (event) {
+  const eventKey = event.key.toLowerCase();
+  const isPressed = pressedKeys.map((e, i) => {
+    if (e === eventKey) {
+      pressedKeys.splice(i, 1);
+      return true;
     }
   });
+  if (isPressed) {
+    $(`div[data-key=${eventKey}]`).css("filter", "brightness(100%)");
+  }
+});
+
+$(document).keypress(function (event) {
+  const eventKey = event.key.toLowerCase();
+  if (pressedKeys.indexOf(eventKey) === -1) {
+    keys.forEach((key) => {
+      if (eventKey === key) {
+        $(`div[data-key=${key}]`).css("filter", "brightness(140%)");
+        pressedKeys.push(key);
+      }
+    });
+  }
 });
