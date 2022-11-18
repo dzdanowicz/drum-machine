4956fb6c33a82dda4b8a4a228e1a5f6c3bd85bb1
dzdanowicz <contact@dzdanowicz.com>
2022-11-18 20:36:58 +0100

    Pressing keys performs an action on the correct div Squashed commit of the following:
    commit 9d6f4acd3219b10eecc3a94c53311b4faa5e2895
    Author: dzdanowicz <contact@dzdanowicz.com>
    Date:   Fri Nov 18 19:58:40 2022 +0100
    
        Pressing keys performs an action on the correct div
        Fix for holding key, holding multiple keys and uppercase input
    
    commit cbac820a9a2e4a599eddb4992c0f0bd4ee409408
    Author: dzdanowicz <contact@dzdanowicz.com>
    Date:   Sun Nov 13 18:20:52 2022 +0100
    
        Change reading keys
        from event.which numbers to
        event.key letters and change data-num to data-key
    
    commit 0f6c2077bcbb70e04a13a26734bce955b6ae97c5
    Author: dzdanowicz <contact@dzdanowicz.com>
    Date:   Sun Nov 13 14:27:15 2022 +0100
    
        Pressing key takes assigned to key div and pass it to the active function
    
    commit db4c2a88926ffaf1325fb0c84ebc5d09e33f7bde
    Author: dzdanowicz <contact@dzdanowicz.com>
    Date:   Sun Nov 13 13:59:07 2022 +0100
    
        Fix data-num numbers to correct ones

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 import { set } from "/sounds/sets.js";
 
-const $pads = $(".drum-pad");
-const keys = [81, 87, 69, 65, 83, 68, 90, 88, 67];
+const keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
 
 for (const [key, value] of Object.entries(set.sounds)) {
   $("#" + key)[0].src = value;
 }
 
-$(document).keypress(function (event) {});
+let pressedKeys = [];
+
+$(document).on("keyup", function (event) {
+  const eventKey = event.key.toLowerCase();
+  const isPressed = pressedKeys.map((e, i) => {
+    if (e === eventKey) {
+      pressedKeys.splice(i, 1);
+      return true;
+    }
+  });
+  if (isPressed) {
+    $(`div[data-key=${eventKey}]`).css("filter", "brightness(100%)");
+  }
+});
+
+$(document).keypress(function (event) {
+  const eventKey = event.key.toLowerCase();
+  if (pressedKeys.indexOf(eventKey) === -1) {
+    keys.forEach((key) => {
+      if (eventKey === key) {
+        $(`div[data-key=${key}]`).css("filter", "brightness(140%)");
+        pressedKeys.push(key);
+      }
+    });
+  }
+});
