e81007d5b3f85c9e4c860057ac656bbee6ff0481
dzdanowicz <contact@dzdanowicz.com>
2022-12-03 14:35:49 +0100

    Create folders for both sound sets in sound folder to store their sounds Change paths in sets.js Remove a3.mp3 and g-5.mp3 and add a-4.mp3 and g-4.mp3

--- sounds/sets.js
+++ sounds/sets.js
@@ -4,39 +4,39 @@ const sets = [
     sounds: {
       q: {
         name: "Clap",
-        path: "sounds/clap.mp3",
+        path: "sounds/drum/clap.mp3",
       },
       w: {
         name: "Closed Hi-hat",
-        path: "sounds/closed-hihat.mp3",
+        path: "sounds/drum/closed-hihat.mp3",
       },
       e: {
         name: "Open Hi-hat",
-        path: "sounds/open-hihat.mp3",
+        path: "sounds/drum/open-hihat.mp3",
       },
       a: {
         name: "Splash Cymbal",
-        path: "sounds/splash-cymbal.mp3",
+        path: "sounds/drum/splash-cymbal.mp3",
       },
       s: {
         name: "Ride Cymbal",
-        path: "sounds/ride-cymbal.mp3",
+        path: "sounds/drum/ride-cymbal.mp3",
       },
       d: {
         name: "Crash Cymbal",
-        path: "sounds/crash-cymbal.mp3",
+        path: "sounds/drum/crash-cymbal.mp3",
       },
       z: {
         name: "Kick",
-        path: "sounds/kick.mp3",
+        path: "sounds/drum/kick.mp3",
       },
       x: {
         name: "Tom",
-        path: "sounds/tom.mp3",
+        path: "sounds/drum/tom.mp3",
       },
       c: {
         name: "Snare",
-        path: "sounds/snare.mp3",
+        path: "sounds/drum/snare.mp3",
       },
     },
   },
@@ -44,40 +44,40 @@ const sets = [
     title: "Piano",
     sounds: {
       q: {
-        name: "A3",
-        path: "sounds/a3.mp3",
+        name: "A4",
+        path: "sounds/piano/a4.mp3",
       },
       w: {
-        name: "A4",
-        path: "sounds/a4.mp3",
+        name: "A-4",
+        path: "sounds/piano/a-4.mp3",
       },
       e: {
         name: "B4",
-        path: "sounds/b4.mp3",
+        path: "sounds/piano/b4.mp3",
       },
       a: {
         name: "C4",
-        path: "sounds/c4.mp3",
+        path: "sounds/piano/c4.mp3",
       },
       s: {
         name: "D4",
-        path: "sounds/d4.mp3",
+        path: "sounds/piano/d4.mp3",
       },
       d: {
         name: "E4",
-        path: "sounds/e4.mp3",
+        path: "sounds/piano/e4.mp3",
       },
       z: {
         name: "F4",
-        path: "sounds/f4.mp3",
+        path: "sounds/piano/f4.mp3",
       },
       x: {
         name: "G4",
-        path: "sounds/g4.mp3",
+        path: "sounds/piano/g4.mp3",
       },
       c: {
-        name: "G-5",
-        path: "sounds/g-5.mp3",
+        name: "G-4",
+        path: "sounds/piano/g-4.mp3",
       },
     },
   },
